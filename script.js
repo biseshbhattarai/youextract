224df84d1e7d96c376cb0a0148eabb5a6ea54f77
biseshbhattarai <aashishbhattarai49@gmail.com>
2020-03-27 11:57:27 +0545

    Extension feature

---
+++ script.js
@@ -0,0 +1,16 @@
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    alert(getdata(url));
+    new_url = getdata(url);
+    const Http = new XMLHttpRequest();
+	const url_='http://127.0.0.1:5000/'+new_url+'/';
+	Http.open("GET", url_);
+	Http.send();
+	Http.onreadystatechange = (e) => {
+  		console.log(Http.responseText)
+  	}
+});
+
+function getdata(url){
+	return url.split('=')[1];
+}
---
+++ script.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import youtube_dl
+import sys
+lists = []
+
+
+app = Flask(__name__)
+
+def run(url):
+	complete_url = 'https://youtube.com/watch?v='+url
+	print(complete_url)
+	ydl_opts = {}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		try:
+			ydl.download([complete_url])
+		except:
+			pass
+
+@app.route('/<url>/', methods=['GET'])
+def home(url):
+	url_ = url
+	print(url) 
+	run(url)
+	return "Downloaded.."
